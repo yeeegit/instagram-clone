9f9407e17e395d076157ebea60aa73e9d0077805
latids <btnygtg@gmail.com>
2024-10-26 03:27:34 +0300

    added media filetype and url sections to post model

--- Backend/src/models/Post.js
+++ Backend/src/models/Post.js
@@ -1,40 +1,48 @@
-const sequelize = require('../../config/connectionToDB')
-const { DataTypes } = require('sequelize')
-const User = require('./User')
+const sequelize = require("../../config/connectionToDB");
+const { DataTypes } = require("sequelize");
+const User = require("./User");
 
-const Post = sequelize.define("Post", {
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  caption: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-  isSaved: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: User,
-      key: 'id',
-    },
-    onDelete: 'CASCADE',
-    allowNull: false
-  }
-
-},
+const Post = sequelize.define(
+  "Post",
   {
-    timestamps: true
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    mediaUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mediaType: {
+      type: DataTypes.ENUM("image", "video"),
+      allowNull: false,
+    },
+    caption: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    isSaved: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: "id",
+      },
+      onDelete: "CASCADE",
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
   }
-)
+);
 
+User.hasMany(Post, { foreignKey: "userId" });
+Post.belongsTo(User, { foreignKey: "userId" });
 
-User.hasMany(Post, { foreignKey: 'userId', })
-Post.belongsTo(User, { foreignKey: 'userId' })
-
-module.exports=Post
+module.exports = Post;
